df12a44f3f8b5e4dbc767ef16eddcf898ecba28e
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2023-06-26 23:06:37 +0300

    remove existing modules before readding

--- contract/lib/foundry-huff/src/test/interfaces/IConstructor.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-pragma solidity >=0.7.0 <0.9.0;
-
-interface IConstructor {
-    function getArgOne() external returns (address);
-    function getArgTwo() external returns (uint256);
-}
