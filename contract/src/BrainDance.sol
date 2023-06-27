b4093fc049b088eb8c404116852a4b6207fe70bd
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2023-06-27 16:50:44 +0300

    cleanup braindance contract

--- contract/src/BrainDance.sol
+++ contract/src/BrainDance.sol
@@ -1,46 +1,55 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../interfaces/IERC20.sol";
 import "v2-core/interfaces/IUniswapV2Pair.sol";
 import "v2-periphery/interfaces/IUniswapV2Router02.sol";
 import "v3-core/interfaces/IUniswapV3Pool.sol";
-import "./lib/SafeMath.sol";
+import "solmate/tokens/ERC20.sol";
 
+/// @title BrainDance
+/// @author 0xmouseless
+/// @notice Minimal swap router used to sim V2/V3 swaps (and account for taxed tokens)
 contract BrainDance {
-    using SafeMath for uint;
-
-    function calculateSwapV2(uint amountIn, address targetPair, address inputToken, address outputToken) external returns (uint amountOut, uint realAfterBalance){
+    /**
+     * @notice Performs a token swap on a v2 pool
+     * @return amountOut Expected output tokens from the swap
+     * @return realAfterBalance Post-swap balance, accounting for token tax
+     */
+    function calculateSwapV2(uint256 amountIn, address targetPair, address inputToken, address outputToken)
+        external
+        returns (uint256 amountOut, uint256 realAfterBalance)
+    {
         //////////////////////////////////////
-        //            NO STRINGS            //
+        //              SETUP               //
         //////////////////////////////////////
 
         // Optimistically send amountIn of inputToken to targetPair
-        IERC20(inputToken).transfer(targetPair, amountIn);
+        ERC20(inputToken).transfer(targetPair, amountIn);
 
         //////////////////////////////////////
         //  CALCULATING OUR EXPECTED OUTPUT //
         //////////////////////////////////////
 
         // Prepare variables for calculating expected amount out
-        uint reserveIn;
-        uint reserveOut;
+        uint256 reserveIn;
+        uint256 reserveOut;
 
-        { // Avoid stack too deep error
-        (uint reserve0, uint reserve1,) = IUniswapV2Pair(targetPair).getReserves();
+        {
+            // Avoid stack too deep error
+            (uint256 reserve0, uint256 reserve1,) = IUniswapV2Pair(targetPair).getReserves();
 
-        // sort reserves
-        if (inputToken < outputToken) {
-            // Token0 is equal to inputToken
-            // Token1 is equal to outputToken
-            reserveIn = reserve0;
-            reserveOut = reserve1;
-        } else {
-            // Token0 is equal to outputToken
-            // Token1 is equal to inputToken
-            reserveIn = reserve1;
-            reserveOut = reserve0;
-        }
+            // sort reserves
+            if (inputToken < outputToken) {
+                // Token0 is equal to inputToken
+                // Token1 is equal to outputToken
+                reserveIn = reserve0;
+                reserveOut = reserve1;
+            } else {
+                // Token0 is equal to outputToken
+                // Token1 is equal to inputToken
+                reserveIn = reserve1;
+                reserveOut = reserve0;
+            }
         }
 
         //////////////////////////////////////
@@ -48,27 +57,27 @@ contract BrainDance {
         //////////////////////////////////////
 
         // Find the actual amountIn sent to pair (accounts for tax if any) and amountOut
-        uint actualAmountIn = IERC20(inputToken).balanceOf(address(targetPair)).sub(reserveIn);
+        uint256 actualAmountIn = ERC20(inputToken).balanceOf(address(targetPair)) - reserveIn;
         amountOut = _getAmountOut(actualAmountIn, reserveIn, reserveOut);
 
         // Prepare swap variables and call pair.swap()
-        (uint amount0Out, uint amount1Out) = inputToken < outputToken ? (uint(0), amountOut) : (amountOut, uint(0));
+        (uint256 amount0Out, uint256 amount1Out) =
+            inputToken < outputToken ? (uint256(0), amountOut) : (amountOut, uint256(0));
         IUniswapV2Pair(targetPair).swap(amount0Out, amount1Out, address(this), new bytes(0));
 
         // Find real balance after (accounts for taxed tokens)
-        realAfterBalance = IERC20(outputToken).balanceOf(address(this));
+        realAfterBalance = ERC20(outputToken).balanceOf(address(this));
     }
 
-    function _getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) internal pure returns (uint amountOut) {
-        require(amountIn > 0, 'UniswapV2Library: INSUFFICIENT_INPUT_AMOUNT');
-        require(reserveIn > 0 && reserveOut > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
-        uint amountInWithFee = amountIn.mul(997);
-        uint numerator = amountInWithFee.mul(reserveOut);
-        uint denominator = reserveIn.mul(1000).add(amountInWithFee);
-        amountOut = numerator / denominator;
-    }
-
-    function calculateSwapV3(int amountIn, address targetPoolAddress, address inputToken, address outputToken) public returns (uint amountOut, uint realAfterBalance) {
+    /**
+     * @notice Performs a token swap on a v3 pool
+     * @return amountOut Expected output tokens from the swap
+     * @return realAfterBalance Post-swap balance, accounting for token tax
+     */
+    function calculateSwapV3(int256 amountIn, address targetPoolAddress, address inputToken, address outputToken)
+        public
+        returns (uint256 amountOut, uint256 realAfterBalance)
+    {
         IUniswapV3Pool targetPool = IUniswapV3Pool(targetPoolAddress);
         // wether tokenIn is token0 or token1
         bool zeroForOne = inputToken < outputToken;
@@ -81,25 +90,42 @@ contract BrainDance {
         bytes memory data = abi.encode(zeroForOne, inputToken);
 
         // Make swap and calc amountOut
-        (int amount0, int amount1) = targetPool.swap(address(this), zeroForOne, amountIn, sqrtPriceLimitX96, data);
+        (int256 amount0, int256 amount1) = targetPool.swap(address(this), zeroForOne, amountIn, sqrtPriceLimitX96, data);
         amountOut = uint256(-(zeroForOne ? amount1 : amount0));
 
         // Find real balance after (accounts for taxed tokens)
-        realAfterBalance = IERC20(outputToken).balanceOf(address(this));
+        realAfterBalance = ERC20(outputToken).balanceOf(address(this));
     }
 
-    function uniswapV3SwapCallback(
-        int256 amount0Delta,
-        int256 amount1Delta,
-        bytes calldata _data
-    ) external {
+    /**
+     * @notice Post swap callback to sends amount of input token to v3 pool
+     */
+    function uniswapV3SwapCallback(int256 amount0Delta, int256 amount1Delta, bytes calldata _data) external {
         require(amount0Delta > 0 || amount1Delta > 0); // swaps entirely within 0-liquidity regions are not supported
         (bool isZeroForOne, address inputToken) = abi.decode(_data, (bool, address));
 
         if (isZeroForOne) {
-            IERC20(inputToken).transfer(msg.sender, uint(amount0Delta));
+            ERC20(inputToken).transfer(msg.sender, uint256(amount0Delta));
         } else {
-            IERC20(inputToken).transfer(msg.sender, uint(amount1Delta));
+            ERC20(inputToken).transfer(msg.sender, uint256(amount1Delta));
         }
+    }
+
+    /**
+     * @notice Helper to find output amount from xy=k
+     * @dev Note that fees are hardcoded to 0.3% (default for sushi and uni)
+     * @return amountOut Output tokens expected from swap
+     */
+    function _getAmountOut(uint256 amountIn, uint256 reserveIn, uint256 reserveOut)
+        internal
+        pure
+        returns (uint256 amountOut)
+    {
+        require(amountIn > 0, "UniswapV2Library: INSUFFICIENT_INPUT_AMOUNT");
+        require(reserveIn > 0 && reserveOut > 0, "UniswapV2Library: INSUFFICIENT_LIQUIDITY");
+        uint256 amountInWithFee = amountIn * 997;
+        uint256 numerator = amountInWithFee * reserveOut;
+        uint256 denominator = reserveIn * 1000 + amountInWithFee;
+        amountOut = numerator / denominator;
     }
 }
--- contract/test/BrainDance.t.sol
+++ contract/test/BrainDance.t.sol
@@ -2,73 +2,124 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
-import "forge-std/Vm.sol";
-import "forge-std/console.sol";
-import { IWETH } from "../interfaces/IWETH.sol";
+import "forge-std/console2.sol";
 import "v2-core/interfaces/IUniswapV2Pair.sol";
 import "v2-core/interfaces/IUniswapV2Factory.sol";
 import "v2-periphery/interfaces/IUniswapV2Router02.sol";
+import "v3-periphery/interfaces/IQuoter.sol";
+import "v3-core/interfaces/IUniswapV3Pool.sol";
+import "solmate/tokens/WETH.sol";
 
 import "../src/BrainDance.sol";
 
+/// @title BrainDanceTest
+/// @author 0xmouseless
+/// @notice Test suite for the BrainDance contract
 contract BrainDanceTest is Test {
+    address constant weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    IUniswapV2Factory uniV2Factory;
+    IUniswapV2Router02 uniV2Router;
+    IQuoter uniV3Quoter;
     BrainDance brainDance;
-    IWETH weth = IWETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-    IERC20 usdc = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
-    address wethUsdcPairV2;
-
-    IUniswapV2Factory univ2Factory = IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f);
 
     /// @notice Set up the testing suite
     function setUp() public {
-        // Deposit ether into WETH Contract
-        weth.deposit{value: 10e18}();
-
         brainDance = new BrainDance();
 
-        wethUsdcPairV2 = _getPairV2(address(weth), address(usdc));
+        uniV2Factory = IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f);
+        uniV2Router = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
+        uniV3Quoter = IQuoter(0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6);
+        WETH wrappedEther = WETH(payable(weth));
 
-        // Transfer the weth to the sandwich contract
-        weth.transfer(address(brainDance), 10e18);
+        wrappedEther.deposit{value: 10e18}();
+        wrappedEther.transfer(address(brainDance), 10e18);
     }
 
-    function testSwapWethUsdcUniswapV3() public {
-        address usdcWethPoolV3_05fee = 0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640;
-        brainDance.calculateSwapV3(2 ether, usdcWethPoolV3_05fee, address(weth), 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
-        uint balance = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48).balanceOf(address(brainDance));
-        console.log("balance: ", balance);
+    function testUniswapV3() public {
+        address usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+        address usdcWethPool = 0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640; // 500 fee pool
+
+        // swapping 2 weth to usdc
+        int256 amountIn = 2 ether;
+        uint256 amountOutExpected = _quoteV3Swap(amountIn, usdcWethPool, weth, usdc);
+        (uint256 amountOut,) = brainDance.calculateSwapV3(amountIn, usdcWethPool, weth, usdc);
+        console2.log("swapped %d WETH for %d USDC", uint256(amountIn), amountOut);
+        assertEq(
+            amountOutExpected, amountOut, "WETH->USDC swap failed: received USDC deviates from expected router output."
+        );
+
+        // swapping received usdc back to weth
+        amountIn = int256(amountOut);
+        amountOutExpected = _quoteV3Swap(amountIn, usdcWethPool, usdc, weth);
+        (amountOut,) = brainDance.calculateSwapV3(amountIn, usdcWethPool, usdc, weth);
+        console2.log("swapped %d USDC for %d WETH", uint256(amountIn), amountOut);
+        assertEq(
+            amountOutExpected, amountOut, "USDC->WETH swap failed: received WETH deviates from expected router output."
+        );
     }
 
-    function testSwapWethUsdtUniswapV3() public {
-        address wethUsdtPoolV3_30fee = 0x4e68Ccd3E89f51C3074ca5072bbAC773960dFa36;
-        brainDance.calculateSwapV3(2 ether, wethUsdtPoolV3_30fee, address(weth), 0xdAC17F958D2ee523a2206206994597C13D831ec7);
-        uint balance = IERC20(0xdAC17F958D2ee523a2206206994597C13D831ec7).balanceOf(address(brainDance));
-        console.log("balance: ", balance);
-    }
+    function testUniswapV2() public {
+        address usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+        address usdcWethPair = _getPairUniV2(usdc, address(weth));
 
-    function testSwapWethUsdcUniswapV2() public {
-        (uint amountOut,) = brainDance.calculateSwapV2(2 ether, wethUsdcPairV2, address(weth), 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
-        console.log("amountOut:", amountOut);
-    }
+        // swapping 2 weth to usdc
+        uint256 amountIn = 2 ether;
+        uint256 amountOutExpected = _quoteV2Swap(amountIn, usdcWethPair, weth < usdc);
+        (uint256 amountOut,) = brainDance.calculateSwapV2(amountIn, usdcWethPair, weth, usdc);
+        console2.log("swapped %d WETH for %d USDC", amountIn, amountOut);
+        assertEq(
+            amountOutExpected, amountOut, "WETH->USDC swap failed: received USDC deviates from expected router output."
+        );
 
-    function testSwapTaxedTokenUniswapV2() public {
-        address wagie = 0x492baa7A6450712D4bbCCa01B87F029DEe3Ea3Ec;
-        address wagieWethPair = _getPairV2(address(weth), wagie);
-        (uint amountOut,) = brainDance.calculateSwapV2(2 ether, wagieWethPair, address(weth), 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
-        console.log("amountOut:", amountOut);
-    }
-
-    function testSwapTokenSushiSwap() public {
-        (uint amountOut,) = brainDance.calculateSwapV2(2 ether, 0x397FF1542f962076d0BFE58eA045FfA2d347ACa0, address(weth), 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
-        console.log("amountOut:", amountOut);
+        // swapping received usdc back to weth
+        amountIn = amountOut;
+        amountOutExpected = _quoteV2Swap(amountIn, usdcWethPair, usdc < weth);
+        (amountOut,) = brainDance.calculateSwapV2(amountIn, usdcWethPair, usdc, weth);
+        console2.log("swapped %d USDC for %d WETH", amountIn, amountOut);
+        assertEq(
+            amountOutExpected, amountOut, "USDC->WETH swap failed: received WETH deviates from expected router output."
+        );
     }
 
     function testGetBrainDanceCode() public {
-      bytes memory code = address(brainDance).code;
-      emit log_bytes(code);
+        bytes memory code = address(brainDance).code;
+        emit log_bytes(code);
     }
 
-    function _getPairV2(address tokenA, address tokenB) private view returns (address) {
-        return univ2Factory.getPair(tokenA, tokenB);
+    // HELPERS
+    function _quoteV3Swap(int256 amountIn, address _pool, address tokenIn, address tokenOut)
+        private
+        returns (uint256 amountOut)
+    {
+        IUniswapV3Pool pool = IUniswapV3Pool(_pool);
+
+        // wether tokenIn is token0 or token1
+        bool zeroForOne = tokenIn < tokenOut;
+        // From docs: The Q64.96 sqrt price limit. If zero for one,
+        // The price cannot be less than this value after the swap.
+        // If one for zero, the price cannot be greater than this value after the swap
+        uint160 sqrtPriceLimitX96 = (zeroForOne ? 4295128749 : 1461446703485210103287273052203988822378723970341);
+
+        amountOut =
+            uniV3Quoter.quoteExactInputSingle(tokenIn, tokenOut, pool.fee(), uint256(amountIn), sqrtPriceLimitX96);
+    }
+
+    function _quoteV2Swap(uint256 amountIn, address pair, bool isInputToken0)
+        private
+        view
+        returns (uint256 amountOut)
+    {
+        (uint256 reserveIn, uint256 reserveOut,) = IUniswapV2Pair(pair).getReserves();
+
+        if (!isInputToken0) {
+            // reserveIn is token1
+            (reserveIn, reserveOut) = (reserveOut, reserveIn);
+        }
+
+        amountOut = uniV2Router.getAmountOut(amountIn, reserveIn, reserveOut);
+    }
+
+    function _getPairUniV2(address tokenA, address tokenB) private view returns (address pair) {
+        pair = uniV2Factory.getPair(tokenA, tokenB);
     }
 }
