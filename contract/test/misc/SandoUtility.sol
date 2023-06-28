549798ffeb035b034232f5c4c7723a6febf5fdc9
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2023-06-28 16:00:46 +0300

    cleanup variable names

--- contract/test/misc/SandoUtility.sol
+++ contract/test/misc/SandoUtility.sol
@@ -19,21 +19,21 @@ library SandoUtility {
      * @param functionName The name of the function we want to jump to
      * @return JUMPDEST location in bytecode
      */
-    function getJumpDestFromSig(string memory functionName) public view returns (uint8) {
+    function getJumpDestFromSig(string memory functionName) public pure returns (uint8) {
         uint8 startingIndex = 0x06;
 
         // array mapped in same order as on sando contract
         string[13] memory functionNames = [
-            "v2_output0",
-            "v2_input0",
-            "v2_output1",
-            "v2_input1",
-            "v3_output1_big",
-            "v3_output0_big",
-            "v3_output1_small",
-            "v3_output0_small",
-            "v3_input0",
-            "v3_input1",
+            "v2_backrun0",
+            "v2_frontrun0",
+            "v2_backrun1",
+            "v2_frontrun1",
+            "v3_backrun1_big",
+            "v3_backrun0_big",
+            "v3_backrun1_small",
+            "v3_backrun0_small",
+            "v3_frontrun0",
+            "v3_frontrun1",
             "seppuku",
             "recoverEth",
             "recoverWeth"
@@ -51,25 +51,28 @@ library SandoUtility {
     }
 
     /**
-     * @notice Encodes a given value to 5 bytes of calldata
+     * @notice Encodes the other token value to 5 bytes of calldta
+     * @dev For frontrun, otherTokenValue indicates swapAmount (pool's amountOut)
+     * @dev For backrun, otherTokenValue indicates amount to transfer to pool (amountIn)
      * @dev 4 bytes reserved for encodeValue
      * @dev 1 byte reserved for storage slot to store in
+     *
      * @param amount The amount to be encoded
-     * @param isWethInput A flag indicating if the input token is WETH
+     * @param isTxFrontrun A flag indicating if the input token is WETH (frontrun)
      * @param isWethToken0 A flag indicating if the token0 is WETH
      * @return fourByteValue The encoded amount (4 byte)
      * @return oneByteMemOffset The calculated byte offset for the encoded amount (1 byte)
      * @return realAmountAfterEncoding The amount after encoding, shifted by the byte offset
      */
-    function encodeToFiveBytes(uint256 amount, bool isWethInput, bool isWethToken0)
+    function encodeOtherTokenToFiveBytes(uint256 amount, bool isTxFrontrun, bool isWethToken0)
         public
         pure
         returns (uint32 fourByteValue, uint8 oneByteMemOffset, uint256 realAmountAfterEncoding)
     {
-        uint256 numBytesToEncodeTo = 4;
+        uint8 numBytesToEncodeTo = 4;
 
-        for (uint256 i = 0; i < 32; i++) {
-            uint256 _encodedAmount = amount / 2 ** (8 * i);
+        for (uint8 i = 0; i < 32; i++) {
+            uint32 _encodedAmount = uint32(amount) / uint32(2) ** uint32((8 * i));
 
             // If we can fit the value in numBytesToEncodeTo bytes, we can encode it
             if (_encodedAmount <= 2 ** (numBytesToEncodeTo * (8)) - 1) {
@@ -81,13 +84,41 @@ library SandoUtility {
             }
         }
 
-        if (!isWethInput) {
-            // find byte placement for Transfer(address,uint256)
+        if (!isTxFrontrun) {
+            /* sando MEMORY DUMP for when we call otherToken's `transfer(to,amount)` method
+            0x00: 0x0000000000000000000000000000000000000000000000000000000000000000
+            0x20: 0x00000000????????????????????????????????????????????????????????
+            0x40: 0x????????00000000000000000000000000000000000000000000000000000000
+            ...
+
+            second param of `transer(to,amount)` takes up the region marked with `?`,
+            meaning that to find byteshift, we subtract from memory slot 0x44 (68 in dec)
+            */
             oneByteMemOffset = 68 - numBytesToEncodeTo - oneByteMemOffset;
         } else {
             if (isWethToken0) {
+                /* MEMORY DUMP for when we call `swap(amount0Out,amount1Out,to,bytes)` on sando contract
+                0x00: 0x0000000000000000000000000000000000000000000000000000000000000000
+                0x20: 0x00000000????????????????????????????????????????????????????????
+                0x40: 0x????????00000000000000000000000000000000000000000000000000000000
+                0x60: 0x0000000000000000000000000000000000000000000000000000000000000000
+                ...
+
+                weth is token0, otherToken is token1, so otherToken amountOut takes up the region marked with `?` (amount1Out).
+                meaning that to find byteshift, we subtract from memory slot 0x44 (68 in dec)
+                */
                 oneByteMemOffset = 68 - numBytesToEncodeTo - oneByteMemOffset;
             } else {
+                /* MEMORY DUMP for when we call `swap(amount0Out,amount1Out,to,bytes)` on sando contract
+                0x00: 0x0000000?????????????????????????????????????????????????????????
+                0x20: 0x???????000000000000000000000000000000000000000000000000000000000
+                0x40: 0x0000000000000000000000000000000000000000000000000000000000000000
+                0x60: 0x0000000000000000000000000000000000000000000000000000000000000000
+                ...
+
+                weth is token1, otherToken is token0, so otherToken amountOut takes up the region marked with `?` (amount0Out).
+                meaning that to find byteshift, we subtract from memory slot 0x24 (36 in dec)
+                */
                 oneByteMemOffset = 36 - numBytesToEncodeTo - oneByteMemOffset;
             }
         }
@@ -96,7 +127,7 @@ library SandoUtility {
     /**
      * @notice Utility function to create payload for our v3 frontruns
      * @return payload Calldata bytes to execute frontrun
-     * @return encodedValue Encoded `tx.value` to execute frontrun
+     * @return encodedValue Encoded `tx.value` indicating WETH amount to send
      */
     function v3CreateFrontrunPayload(
         address pool,
@@ -104,12 +135,12 @@ library SandoUtility {
         address outputToken,
         uint24 fee,
         int256 amountIn
-    ) public view returns (bytes memory payload, uint256 encodedValue) {
+    ) public pure returns (bytes memory payload, uint256 encodedValue) {
         (address token0, address token1) =
             inputToken < outputToken ? (inputToken, outputToken) : (outputToken, inputToken);
         bytes32 pairInitHash = keccak256(abi.encode(token0, token1, fee));
 
-        uint8 swapType = _v3FindSwapType(true, inputToken, outputToken, amountIn);
+        uint8 swapType = _v3FindSwapType(true, outputToken, amountIn);
         payload = abi.encodePacked(uint8(swapType), address(pool), pairInitHash);
         encodedValue = uint256(amountIn) / wethEncodeMultiple();
     }
@@ -124,12 +155,12 @@ library SandoUtility {
         address outputToken,
         uint24 fee,
         int256 amountIn
-    ) public view returns (bytes memory payload) {
+    ) public pure returns (bytes memory payload) {
         (address token0, address token1) =
             inputToken < outputToken ? (inputToken, outputToken) : (outputToken, inputToken);
         bytes32 pairInitHash = keccak256(abi.encode(token0, token1, fee));
 
-        uint8 swapType = _v3FindSwapType(false, inputToken, outputToken, amountIn);
+        uint8 swapType = _v3FindSwapType(false, outputToken, amountIn);
         if (amountIn <= 281474976710655) {
             // use small method
             payload =
@@ -142,7 +173,12 @@ library SandoUtility {
         }
     }
 
-    function v2CreateSandwichPayloadWethIsOutput(address otherToken, uint256 amountIn)
+    /**
+     * @notice Utility function to create payload for our v2 backruns
+     * @return payload Calldata bytes to execute backruns
+     * @return encodedValue Encoded `tx.value` indicating WETH amount to send
+     */
+    function v2CreateBackrunPayload(address otherToken, uint256 amountIn)
         public
         view
         returns (bytes memory payload, uint256 encodedValue)
@@ -158,7 +194,7 @@ library SandoUtility {
 
         // encode amountIn
         (uint32 encodedAmountIn, uint8 memoryOffset, uint256 amountInActual) =
-            encodeNumToByteAndOffset(amountIn, false, false);
+            encodeOtherTokenToFiveBytes(amountIn, false, false);
 
         payload = abi.encodePacked(
             uint8(swapType), // token we're giving
@@ -172,7 +208,12 @@ library SandoUtility {
         encodedValue = amountOut / wethEncodeMultiple();
     }
 
-    function v2CreateSandwichPayloadWethIsInput(address otherToken, uint256 amountIn)
+    /**
+     * @notice Utility function to create payload for our v2 frontruns
+     * @return payload Calldata bytes to execute frontruns
+     * @return encodedValue Encoded `tx.value` indicating WETH amount to send
+     */
+    function v2CreateFrontrunPayload(address otherToken, uint256 amountIn)
         public
         view
         returns (bytes memory payload, uint256 encodedValue)
@@ -187,7 +228,7 @@ library SandoUtility {
         uint256 amountInActual = (amountIn / wethEncodeMultiple()) * wethEncodeMultiple();
 
         // Get amounts out and encode it
-        (uint256 encodedAmountOut, uint256 memoryOffset,) = encodeNumToByteAndOffset(
+        (uint256 encodedAmountOut, uint256 memoryOffset,) = encodeOtherTokenToFiveBytes(
             GeneralHelper.getAmountOut(weth, otherToken, amountInActual), true, weth < otherToken
         );
 
@@ -205,58 +246,60 @@ library SandoUtility {
     }
 
     // HELPERS
-    function _v3FindSwapType(bool isWethInput, address inputToken, address outputToken, int256 amountIn)
+    function _v3FindSwapType(bool isFrontrunTx, address outputToken, int256 amountIn)
         internal
-        view
+        pure
         returns (uint8)
     {
-        if (isWethInput) {
-            if (inputToken < outputToken) {
+        address weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+        if (isFrontrunTx) {
+            if (weth < outputToken) {
                 // weth is input and token0
-                return getJumpDestFromSig("v3_input0");
+                return getJumpDestFromSig("v3_frontrun0");
             } else {
                 // weth is input and token1
-                return getJumpDestFromSig("v3_input1");
+                return getJumpDestFromSig("v3_frontrun1");
             }
         } else {
-            if (inputToken < outputToken) {
+            if (weth < outputToken) {
                 // weth is output and token1
                 if (amountIn <= 281474976710655) {
                     // && amountIn < 281474976710655 (0xFFFFFFFFFFFF)
-                    return getJumpDestFromSig("v3_output1_small");
+                    return getJumpDestFromSig("v3_backrun1_small");
                 } else {
-                    return getJumpDestFromSig("v3_output1_big");
+                    return getJumpDestFromSig("v3_backrun1_big");
                 }
             } else {
                 // weth is output and token0
                 if (amountIn <= 281474976710655) {
                     // && amountIn < 10000000000000
-                    return getJumpDestFromSig("v3_output0_small");
+                    return getJumpDestFromSig("v3_backrun0_small");
                 } else {
-                    return getJumpDestFromSig("v3_output0_big");
+                    return getJumpDestFromSig("v3_backrun0_big");
                 }
             }
         }
     }
 
-    function _v2FindFunctionSig(bool isWethInput, address inputToken, address otherToken) internal view returns (uint8 encodeAmount) {
+    function _v2FindFunctionSig(bool isFrontrunTx, address otherToken) internal pure returns (uint8 encodeAmount) {
         address weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
-        if (isWethInput) {
+        if (isFrontrunTx) {
             if (weth < otherToken) {
                 // weth is input and token0
-                return getJumpDestFromSig("v2_input0");
+                return getJumpDestFromSig("v2_frontrun0");
             } else {
                 // weth is input and token1
-                return getJumpDestFromSig("v2_input1");
+                return getJumpDestFromSig("v2_frontrun1");
             }
         } else {
             if (weth < otherToken) {
                 // weth is output and token0
-                return getJumpDestFromSig("v2_output0");
+                return getJumpDestFromSig("v2_backrun0");
             } else {
                 // weth is output and token1
-                return getJumpDestFromSig("v2_output1");
+                return getJumpDestFromSig("v2_backrun1");
             }
         }
     }
