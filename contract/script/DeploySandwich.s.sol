f878baa86837bf5580649634bb75c8b5ca768e88
Juice Man <92176188+ElPettego@users.noreply.github.com>
2023-07-06 17:51:31 +0200

    Update DeploySandwich.s.sol

--- contract/script/DeploySandwich.s.sol
+++ contract/script/DeploySandwich.s.sol
@@ -19,4 +19,4 @@ contract Deployer is Script {
     }
 }
 //run bash
-//forge script ./script/DeploySandwich.s.sol --rpc-url http://127.0.0.1:8545 --broadcast --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+//forge script ./script/DeploySandwich.s.sol --rpc-url http://127.0.0.1:8545 --broadcast --private-key PRIVATE_KEY
